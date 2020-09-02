1e9299dd88e25220618718df5e4307fa22c01c6c
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2020-09-02 16:27:18 -0500

    able to deserialize styles from toml

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,17 @@ use std::fs;
 // use toml_edit::{Document, value};
 use indexmap::IndexMap;
 use toml_edit::Document;
+// use serde::{Serialize, Deserialize};
+// use serde_json::{Result, Value};
+use ansi_term::{Color, Style};
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
     let contents = fs::read_to_string(filename).unwrap();
     let mut doc = contents.parse::<Document>().expect("Invalid TOML file.");
+
     // let green = "\u{1b}[32m";
     // let reset = "\u{1b}[0m";
 
@@ -88,8 +93,59 @@ fn print_value(value: &toml_edit::Value, key: &str, map: &mut IndexMap<String, S
             println!("Boolean)={}", b)
         }
         toml_edit::Value::Array(a) => print_array(&a, key, map),
-        toml_edit::Value::InlineTable(it) => print_table(it, key, map), //println!("InlineTable)={}", it),
+        toml_edit::Value::InlineTable(it) => {
+            let style: IndexMap<&str, String> =
+                it.iter()
+                .map(|(k, v)|
+                (k, v.to_string())).collect();
+            println!("StyleMap = [{:#?}]", &style);
+            let ansi_style = create_style(style);
+            println!("Style=[{:#?}]", ansi_style);
+            print_table(it, key, map) //println!("InlineTable)={}", it),
+        }
     }
+}
+
+fn get_color_from_string(color_str: &str) -> ansi_term::Color {
+    match color_str.as_ref() {
+        "black" => Color::Black,
+        "red" => Color::Red,
+        "green" => Color::Green,
+        "yellow" => Color::Yellow,
+        "blue" => Color::Blue,
+        "purple" => Color::Purple,
+        "cyan" => Color::Cyan,
+        "white" => Color::White,
+        // starts with #
+        // is a number
+        // starts with RGB
+        _ => Color::White,
+    }
+}
+
+fn create_style(style_map: IndexMap<&str, String>) -> Style {
+    let mut style = Style::default();
+    for (k, v) in style_map.iter() {
+        println!("K=[{}] V=[{}]", k, v.trim());
+        let key = k.trim();
+        let value =v.trim().trim_matches('"');
+        match key {
+            "fg" => style = style.fg(get_color_from_string(value)),
+            "bg" => style = style.on(get_color_from_string(value)),
+            "is_bold" => style = if value == "true" { style.bold() } else { style },
+            "is_dimmed" => style = if value == "true" { style.dimmed() } else { style },
+            "is_italic" => style = if value == "true" { style.italic() } else { style },
+            "is_underline" => style = if value == "true" { style.underline() } else { style },
+            "is_blink" => style = if value == "true" { style.blink() } else { style },
+            "is_reverse" => style = if value == "true" { style.reverse() } else { style },
+            "is_hidden" => style = if value == "true" { style.hidden() } else { style },
+            "is_strikethrough" => style = if value == "true" { style.strikethrough() } else { style },
+            _ => (),
+        };
+    }
+
+    println!("InStyle=[{:?}]", &style);
+    style
 }
 
 fn print_array(array: &toml_edit::Array, key: &str, map: &mut IndexMap<String, String>) {
